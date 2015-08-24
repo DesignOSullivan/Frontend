ebfab9091b75bdb01bdee339f6f07b0275439b70
beckett929 <justin@Justins-MacBook-Pro.local>
2015-08-24 16:34:13 -0400

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-// Empty JS for your own code to be here
+$(document).ready(function() {
+	
+    $('a[rel*=external]').click( function() {
+        window.open(this.href);
+        return false;
+    });
+	
+
+	$('#pre-tour-toggle').change(function(){
+		if ($('#pre-tour-tab-link').hasClass('displayNone')) {
+		  	$('#pre-tour-tab-link').removeClass('displayNone');
+		}
+		else { 
+		  	$('#pre-tour-tab-link').addClass('displayNone');
+		}
+		
+	});
+	
+	$('#post-tour-toggle').change(function(){
+		if ($('#post-tour-tab-link').hasClass('displayNone')) {
+		  	$('#post-tour-tab-link').removeClass('displayNone');
+		}
+		else { 
+		  	$('#post-tour-tab-link').addClass('displayNone');
+		}
+		
+	});
+});

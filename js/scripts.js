ab695fbcb12d65c66644d7ff6124e619db118b6a
beckett929 <justin@Justins-MacBook-Pro.local>
2015-08-24 15:53:00 -0400

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-// Empty JS for your own code to be here
+$(document).ready(function() {
+	
+    $('a[rel*=external]').click( function() {
+        window.open(this.href);
+        return false;
+    });
+	
+	$('.fancybox').fancybox();
+	
+	// adding responsive stuff to some basic elements
+	$('table').addClass('table table-striped table-bordered').removeAttr('border').removeAttr('cellpadding').removeAttr('cellspacing').wrap("<div class='table-responsive'></div>");
+	$('iframe').addClass('embed-responsive-item').wrap("<div class='embed-responsive embed-responsive-16by9'></div>");
+	$('embed').addClass('embed-responsive-item').wrap("<div class='embed-responsive embed-responsive-16by9'></div>");
+	$('object').addClass('embed-responsive-item').wrap("<div class='embed-responsive embed-responsive-16by9'></div>");
+	
+	
+	$(function(){
+		$('.dropdown').hover(function() {
+			 $(this).toggleClass('open');
+		});
+	});
+	
+});

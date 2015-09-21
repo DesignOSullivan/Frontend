18bac79f665453feb2bf5cb3bab3a5c15321b5f5
DesignOSullivan <daniel.osullivan@archetypesc.com>
2015-09-21 11:52:07 -0400

    Guest Stuff

--- js/guest-scripts.js
+++ js/guest-scripts.js
@@ -57,3 +57,22 @@ $(document).ready(function() {
 	
 	
 });
+
+$(document).ready(function() {
+    $("#phoneNumber").kendoMaskedTextBox({
+        mask: "(999) 000-0000"
+    });
+
+    $("#credit_card").kendoMaskedTextBox({
+        mask: "0000 0000 0000 0000"
+    });
+
+    $("#ssn").kendoMaskedTextBox({
+        mask: "000-00-0000"
+    });
+
+    $("#postcode").kendoMaskedTextBox({
+        mask: "L0L 0LL"
+    });
+});
+</script>
